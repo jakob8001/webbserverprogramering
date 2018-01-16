5398b10b7f20f7cc62cb37bfff4fa69ff9f5264a
jakobn04@gmail.com <jakobn04@gmail.com>
2018-01-16 16:35:05 +0100

    gjorde så jag kan hovra över den

--- Javascript/upppgift js.php
+++ Javascript/upppgift js.php
@@ -23,6 +23,19 @@
 		
 		<img onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src="kim.jpg" alt="kim" width="32" height="32">
 		
+		<script>
+			function bigImg(x) {
+				x.style.height = "64px";
+				x.style.width = "64px";
+			}
+
+			function normalImg(x) {
+				x.style.height = "32px";
+				x.style.width = "32px";
+			}
+		</script>
+
+		
 		
 		
 		
